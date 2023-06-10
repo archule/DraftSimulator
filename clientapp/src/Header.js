6a6428bc9f693a65c318bf73ff712dcdff07b18d
Kai <karchule@terpmail.umd.edu>
2023-06-10 01:33:10 -0400

    added a header

---
+++ clientapp/src/Header.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+export default class Header extends Component {
+  render() { return (
+        
+          <header>
+            <h1>NFL Draft Simulator</h1>
+          </header>
+        
+ 
+      )
+  }
+}
+
